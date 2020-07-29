28444f22e739cc2631083b4a0d54a1d75478c258
LightyD55 <65380695+LightyD55@users.noreply.github.com>
2020-07-29 11:10:56 +0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Header />
+		<Latest :posts="posts"/>
+		<Posts :posts="posts" :count="postCount" @showMore="this.showMore"/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header';
+import Latest from './components/Latest';
+import Posts from './components/Posts';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	data () {
+		return{
+			API_URL:"http://localhost:3000/api/",
+			posts: [],
+			postCount: 10
+		}
+	},
+	components: { Header, Latest, Posts },
+	methods:{
+		getPosts (){
+			fetch("this.API_URL" + "posts/all")
+			.then(data =>{
+				return data.json();
+			})
+			.then(json =>{
+				this.posts = json.result;
+			});
+		},
+		showMore (){
+			this.postCount += 2;
+		}
+	},
+	beforeMount(){
+		this.getPosts();
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+body {
+	background-color: #F3F3F3;
+	font-family: "Rockwell", serif;
+	h3 {
+		color: #171717;
+		font-size: 28px;
+		font-weight: 700;
+	}
 }
 </style>
